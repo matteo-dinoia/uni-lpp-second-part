e8f49a6ac109402dce655adf85af6948436c2c45
Matteo Di Noia <dmatteo002@gmail.com>
2023-12-05 16:34:00 +0100

    refixing compile sh

--- coq/make.sh
+++ coq/make.sh
@@ -2,7 +2,9 @@ coqc -topfile PositiveNum.v -Q . LPP PositiveNum.v
 coqc -topfile PositiveNumOrder.v -Q . LPP PositiveNumOrder.v
 coqc -topfile PositiveNumVsInteger.v -Q . LPP PositiveNumVsInteger.v
 coqc -topfile BNumFuncApp.v -Q . LPP BNumFuncApp.v
-coqc -topfile BNumFuncAppPropertiesConEsercizi.v -Q . LPP BNumFuncAppPropertiesEsercizi.v
+
+#coqc -topfile BNumFuncAppPropertiesConEsercizi.v -Q . LPP BNumFuncAppPropertiesEsercizi.v
+
 #coqc -topfile MonadID.v -Q . LPP MonadID.v
 #coqc -topfile Bibliografia.v -Q . LPP Bibliografia.v
 
